73f8ae3a3f3792bd1abd8e6625df033c2e431c84
drwankingstein <41176205+drwankingstein@users.noreply.github.com>
2020-04-06 17:51:09 -0400

    Add files via upload

---
+++ ForeverYoungModel.cs
@@ -0,0 +1,64 @@
+﻿//Call out the DLL you mean to edit
+using TaleWorlds.CampaignSystem;
+//Call out the namespace
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+//Call Out DLL Just put it in every module for safety
+using TaleWorlds.Core;
+//Name space lets files interact with eachother easily and used for sorting
+namespace ForeverYoung
+
+{
+    //this Makes a Class and inherits DefaultAgeModel
+	public class ForeverYoungModel : DefaultAgeModel
+	{
+        //overrides the settings
+    public override int BecomeInfantAge
+    {
+      get
+      {
+       //sets the value
+        return 3;
+      }
+    }
+
+    public override int BecomeChildAge
+    {
+      get
+      {
+        return 124;
+      }
+    }
+
+    public override int BecomeTeenagerAge
+    {
+      get
+      {
+        return 125;
+      }
+    }
+
+    public override int HeroComesOfAge
+    {
+      get
+      {
+        return 126;
+      }
+    }
+
+    public override int BecomeOldAge
+    {
+      get
+      {
+        return 127;
+      }
+    }
+
+    public override int MaxAge
+    {
+      get
+      {
+        return 128;
+      }
+    }
+	}
+}
---
+++ ForeverYoungSubModule.cs
@@ -0,0 +1,24 @@
+﻿//same as before, call everything you need to use
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace ForeverYoung
+{
+	public class ForeverYoungSubModule : MBSubModuleBase
+	{
+		//load the module
+		protected override void OnSubModuleLoad()
+		{
+		}
+		//set when to load the module
+		protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+		{
+			//make sure the game is a Campaign mode
+			if (!(game.GameType is Campaign))
+				return;
+			//load the class MUST BE SAME NAMESPACE
+			gameStarterObject.AddModel(new ForeverYoungModel());
+		}
+	}
+}
